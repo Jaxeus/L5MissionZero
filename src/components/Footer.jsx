a4e67ee60cf5aebc62fd854714d50f559b480a43
Jason Cluley <jaxeus@gmail.com>
2023-11-01 15:24:30 +1300

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <h3>©️ Copyright by Jaxeus, 2023.</h3>
+    </div>
+  );
+}
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Main from "./Main";
 import CardSection from "./CardSection";
 import AboutSection from "./AboutSection";
+import Footer from "./Footer";
 
 export default function Homepage() {
   return (
@@ -12,6 +13,7 @@ export default function Homepage() {
       <Main />
       <CardSection />
       <AboutSection />
+      <Footer />
     </div>
   );
 }
